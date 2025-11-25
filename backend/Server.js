20693cea108411706fb0c3306ffa62c28b2ba43d
Ansh Shama <ansh2005sh@gmail.com>
2025-11-26 02:07:25 +0530

    modified:   backend/.env 	modified:   backend/Server.js 	modified:   backend/package.json

--- backend/Server.js
+++ backend/Server.js
@@ -54,7 +54,6 @@ app.use(errorHandler);
 // ==========================
 // Start Server
 // ==========================
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 });
